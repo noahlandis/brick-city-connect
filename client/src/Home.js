4757c0aa881823ae0769b5a6c1d9749601fd93e6
Noah Landis <noahlandis980@gmail.com>
2024-11-19 14:57:24 -0500

    test deployment to staging

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home, automated deployment works!</h1>
             <button onClick={() => navigate('/room')}>
                 Start Chatting
             </button>
