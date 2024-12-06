190acb28f03381c99c44a79b400843691284a9dd
Noah Landis <noahlandis980@gmail.com>
2024-12-06 12:06:37 -0500

    turned duplicate code into function

--- server/src/server.js
+++ server/src/server.js
@@ -13,66 +13,43 @@ const io = new Server(server, {
   upgrade: false
 });
 
-let waitingUser = null; // 
+let waitingUser = null; 
+
+function attemptToMatchUser(socket) {
+  if (waitingUser) { // since we have a waiting user, we can match them
+    console.log('match found. Matching', socket.id, 'with', waitingUser.id);
+
+    // we store a reference to the partner sockets for each user, so when a user leaves, we can tell their partner to find a new match
+    socket.partnerSocket = waitingUser;
+    waitingUser.partnerSocket = socket;
+    socket.emit('match-found', waitingUser.userID);
+    waitingUser.emit('match-found', socket.userID);
+
+    // since they were matched, there's no longer a waiting user
+    waitingUser = null;
+  } else { // we couldn't find a match, so this user is now waiting
+    console.log("couldn't find match: ", socket.id, " is now waiting");
+    waitingUser = socket;
+  }
+
+}
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
   socket.on('join-chat', (userID) => {
     socket.userID = userID;
-    if (waitingUser) {
-      console.log('match found');
-
-      console.log('the id of the waiting user is', waitingUser.userID);
-      console.log('the id of the user who joined is', userID);
-
-
-      // first, we store a reference to the partnerSocket for both users
-      socket.partnerSocket = waitingUser;
-      waitingUser.partnerSocket = socket;
-      // then we emit the match-found event to both users
-      socket.emit('match-found', waitingUser.userID);
-      waitingUser.emit('match-found', userID);
-      // finally, we reset the waitingUser
-      waitingUser = null;
-    } else {
-      console.log('user is waiting since we have nobody to match with');
-      waitingUser = socket;
-    }
-    
+    attemptToMatchUser(socket);
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
 
-    if (socket === waitingUser) {     // if the user is the one waiting, we reset since the waiting user is the one who disconnected
+    if (socket === waitingUser) { // if the user is the one waiting, we reset since the waiting user is the one who disconnected
       waitingUser = null;
     } 
-    else if (socket.partnerSocket) {  // the user is not the waiting user, they have a partner
-      // if there is already a waiting user, we match them
-      if (waitingUser) {
-        console.log('a waiting user is able to be matched.');
-        console.log('the ID of the user still left in the call is', socket.partnerSocket.userID);
-        userStillInChat = socket.partnerSocket;
-        userStillInChat.partnerSocket = waitingUser;
-        waitingUser.partnerSocket = userStillInChat;
-        userStillInChat.emit('match-found', waitingUser.userID);
-        waitingUser.emit('match-found', userStillInChat.userID);
-        waitingUser = null;
-
-       
-
-      }
-      else {
-        console.log('no waiting user can be matched');
-        waitingUser = socket.partnerSocket;
-
-      }
-      
+    else if (socket.partnerSocket) { // the user is not the waiting user, so we try to find a match for the user left in the call
+      attemptToMatchUser(socket.partnerSocket);
     }
-
-    // console.log('the id of the user who left is', socket.userID);
-    // console.log('the id of the user still in the chat is', waitingUser.userID);
-    
   });
 
 });
