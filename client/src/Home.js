9f41ef17fe4369f2c10f442a86cc614da68686f2
Noah Landis <noahlandis980@gmail.com>
2024-12-23 16:04:03 -0500

    sending

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home Testing</h1>
             <button onClick={() => navigate('/chat')}>
                 Start Chatting
             </button>
