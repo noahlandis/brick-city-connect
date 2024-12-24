0ac7842d5978daa364ab2c8cdc21c45b5510fd61
Noah Landis <noahlandis980@gmail.com>
2024-12-24 17:26:41 -0500

    docker for easy use of mailpit (to send emails to localhost) + extracted email logic into service

--- server/src/controllers/verification-code-controller.js
+++ server/src/controllers/verification-code-controller.js
@@ -1,20 +1,9 @@
-// mailgun setup
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+const { sendEmail } = require('../services/email-service');
 
 // verification-code-controller.js
 const verificationCodeController = {
     sendVerificationCode: (req, res) => {
-        console.log("sendVerificationCode was hit. sending email...");
-        mg.messages.create('mg.brickcityconnect.com', {
-            from: "Brick City Connect <noreply@brickcityconnect.com>",
-            to: "noahlandis980@gmail.com", // Replace with the recipient's email
-            subject: "Your Verification Code",
-            text: "This is a test email from brick city connect",
-        })
-        console.log("email sent");
+        sendEmail("noahlandis980@gmail.com", "Your Verification Code", "This is a test email from brick city connect");
     }
 }
 
---
+++ server/src/services/email-service.js
@@ -0,0 +1,44 @@
+// mailgun setup. Although we can use nodemailer, we want to use mailgun's API in production because it supports tracking
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+
+// we use nodemailer for local development so we get emails at localhost:1025
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    host: 'mailpit',  // Change from 'localhost' to 'mailpit'
+    port: 1025,
+    secure: false,
+});
+
+/**
+ * Sends an email to either the recipient or the local mailpit server, depending on the environment.
+ * @param {string} recipient - The email address of the recipient.
+ * @param {string} subject - The subject of the email.
+ * @param {string} text - The body of the email.
+ */
+function sendEmail(recipient, subject, text) {
+    const emailOptions = {
+        from: "Brick City Connect <noreply@brickcityconnect.com>",
+        to: recipient,
+        subject: subject,
+        text: text
+    };
+    if (process.env.ENV === 'local') {
+        sendLocalMail(emailOptions);
+    } else {
+        sendProdMail(emailOptions);
+    }
+}
+
+function sendProdMail(emailOptions) {
+    mg.messages.create('mg.brickcityconnect.com', emailOptions)
+    console.log("prod email sent");
+}
+
+function sendLocalMail(emailOptions) {
+    transporter.sendMail(emailOptions);
+    console.log("local email sent");
+}
+
+module.exports = { sendEmail };
