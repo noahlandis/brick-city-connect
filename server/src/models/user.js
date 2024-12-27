1230cf9b4db9bc0501bca927107676951112ca86
Noah Landis <noahlandis980@gmail.com>
2024-12-27 15:39:53 -0500

    removed columns in user model for now

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -4,8 +4,6 @@ const sequelize = require('../config/database');
 const User = sequelize.define('user', {
   email: DataTypes.STRING,
   password: DataTypes.STRING,
-  token: DataTypes.STRING,
-  isVerified: DataTypes.BOOLEAN,
 });
 
 module.exports = User;
