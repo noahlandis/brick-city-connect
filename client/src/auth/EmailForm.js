8d1d422216f675540531ff86045da47d31c58540
Noah Landis <noahlandis980@gmail.com>
2024-12-29 13:59:38 -0500

    rough email form

--- client/src/auth/EmailForm.js
+++ client/src/auth/EmailForm.js
@@ -9,20 +9,32 @@ function EmailForm() {
     await sendRegisterMagicLink(email);
   }
 
-  return (
-    <div>
-        Enter an Email
-        <input
-        type="email"
-        placeholder="RIT Email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        />
-        <button onClick={handleSendVerification}>Verify Email</button>
+    return (
+        <div>
+            <div className="card-title text-white justify-center font-helvetica">Sign up with email</div>
+            <div className="card-body">
+                <label className="input input-bordered bg-white flex items-center gap-2 text-black" onChange={(e) => setEmail(e.target.value)}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        class="h-4 w-4 opacity-70">
+                        <path
+                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                        <path
+                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input type="text" class="grow" placeholder="user@rit.edu" />
+                </label>
+                <button className="btn btn-sm bg-black text-white" onClick={handleSendVerification}>Continue</button>
+                <p className="mt-4 text-white">
+                Already have an account?{' '}
+                <a className="link font-bold">Sign in</a>
+                </p>
+            </div>
+        </div>
 
-    </div>
-    
-  );
+    );
 }
 
 export default EmailForm;
