2f7d16f1e285f58e16b6bbd566412f3317f31a8d
Noah Landis <noahlandis980@gmail.com>
2024-12-28 20:17:11 -0500

    added tiger logo

--- client/src/layouts/AuthLayout.js
+++ client/src/layouts/AuthLayout.js
@@ -2,6 +2,13 @@ function AuthLayout({ children }) {
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center" style={{ backgroundImage: 'url("/rit.jpg")' }}>
       <div className="bg-[#F76902] p-8 rounded-lg shadow-lg">
+        <div className="flex justify-center mb-6">
+          <img 
+            src="/tiger.png" 
+            alt="RIT Tiger Logo" 
+            className="w-32 h-32"
+          />
+        </div>
         {children}
       </div>
     </div>
