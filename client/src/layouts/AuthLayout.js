2c9c47b4f8e3e4a46f911ab0bb9c23965ecc2c68
Noah Landis <noahlandis980@gmail.com>
2024-12-29 13:30:36 -0500

    header for auth form

--- client/src/layouts/AuthLayout.js
+++ client/src/layouts/AuthLayout.js
@@ -1,13 +1,14 @@
 function AuthLayout({ children }) {
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center" style={{ backgroundImage: 'url("/rit.jpg")' }}>
-      <div className="bg-[#F76902] p-8 rounded-lg shadow-lg">
-        <div className="flex justify-center mb-6">
+      <div className="card bg-[#F76902] p-8 rounded-lg shadow-lg">
+        <div className="flex items-center gap-4">
           <img 
             src="/tiger.png" 
             alt="RIT Tiger Logo" 
             className="w-32 h-32"
           />
+          <div className="card-title text-white font-helvetica text-2xl">Brick City Connect</div>
         </div>
         {children}
       </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'helvetica': ['"Helvetica Neue"', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('daisyui')],
 } 
