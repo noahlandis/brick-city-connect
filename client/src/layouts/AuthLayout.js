11d38a2b2340fce480be9adbc1c333f4d90f6811
Noah Landis <noahlandis980@gmail.com>
2024-12-28 20:12:02 -0500

    added RIT orange for card

--- client/src/layouts/AuthLayout.js
+++ client/src/layouts/AuthLayout.js
@@ -1,7 +1,7 @@
 function AuthLayout({ children }) {
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center" style={{ backgroundImage: 'url("/rit.jpg")' }}>
-      <div className="bg-white bg-opacity-90 p-8 rounded-lg shadow-lg">
+      <div className="bg-[#F76902] p-8 rounded-lg shadow-lg">
         {children}
       </div>
     </div>
