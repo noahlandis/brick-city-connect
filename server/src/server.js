6fd203537861fdb78df7dd6182dfe26f2a03e1fc
Noah Landis <noahlandis980@gmail.com>
2025-01-08 12:12:09 -0500

    email template working with logo

--- server/src/server.js
+++ server/src/server.js
@@ -31,6 +31,7 @@ app.use('/api', authRoutes);
 // Apply Bugsnag middleware
 app.use(middleware.requestHandler);
 app.use(middleware.errorHandler);
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'email-templates'));
