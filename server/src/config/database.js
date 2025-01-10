557e249240e7b64535c32fcd36305d5ad1db741e
Noah Landis <noahlandis980@gmail.com>
2025-01-10 18:54:06 -0500

    test with SSL enforced

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,23 +3,28 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,              // Enforce SSL
+      rejectUnauthorized: false   // For self-signed or AWS certs
+    }
+  }
 });
 
-// Initialize database connection
 async function initializeDatabase() {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
     
-    await sequelize.sync({force: true});
+    await sequelize.sync({ force: true });
     console.log('Database & tables created!');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-};
+}
 
 module.exports = {
   initializeDatabase,
   sequelize
-}; 
+};
