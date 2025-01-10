6c5233429286385a4da317bbd678518147524287
Noah Landis <noahlandis980@gmail.com>
2025-01-10 13:07:00 -0500

    added some documentation to validate-request

--- server/src/middleware/validate-request.js
+++ server/src/middleware/validate-request.js
@@ -1,5 +1,12 @@
 const { validationResult } = require('express-validator');
 
+/**
+ * Since express-validator doesn't automatically return a 400 error, we use this middleware to do so.
+ * @param {*} req - The request object.
+ * @param {*} res 
+ * @param {*} next 
+ * @returns 
+ */
 const validateRequest = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
