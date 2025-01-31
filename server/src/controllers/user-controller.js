a7afbd182fde868c71bbd97f751416fb74166a2b
Noah Landis <noahlandis980@gmail.com>
2025-01-31 17:05:37 -0500

    got user with backgrounds attatched

--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -29,8 +29,32 @@ const userController = {
     },
     
     getUser: async (req, res) => {
-        const { id } = req.params;
-        const user = await User.findByPk(id);
+      const { id } = req.params;  
+      console.log("Getting backgrounds for user", id);
+      let user = await User.findByPk(id, {include: Background});
+
+        // we get all the backgrounds
+        const allBackgrounds = await Background.findAll(
+          {
+            order: [['requiredLevel', 'ASC']],
+            attributes: ['id', 'name', 'url'] // we exclude createdAt and updatedAt
+          }
+        );
+
+        // we get the user's unlocked backgrounds
+        const userBackgrounds = await user.getBackgrounds();
+
+        // Then we map all the backgrounds and mark them as locked/unlocked.
+        // Note that although we know the user's level and the required level for each background,
+        // this allows us for flexibility if we gift backgrounds to users.
+        const backgrounds = allBackgrounds.map(background => {
+            return {
+                ...background.toJSON(), // Convert Sequelize instance to plain object
+                locked: !userBackgrounds.some(userBackground => userBackground.id === background.id),
+            };
+        });
+        user = user.toJSON();
+        user.backgrounds = backgrounds;        
         return res.status(200).json(user);
     }
 }
