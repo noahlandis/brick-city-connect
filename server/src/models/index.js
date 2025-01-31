64cb923a3f9748e86a316b2c4ce2c0e213d64078
Noah Landis <noahlandis980@gmail.com>
2025-01-30 20:52:31 -0500

    no longer explicitly defining model

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,8 +1,7 @@
 const User = require('./user');
 const Background = require('./background');
-const User_Background = require('./user-background');
 
-User.belongsToMany(Background, { through: User_Background });
-Background.belongsToMany(User, { through: User_Background });
+User.belongsToMany(Background, { through: 'user_backgrounds' });
+Background.belongsToMany(User, { through: 'user_backgrounds' });
 
-module.exports = { User, Background, User_Background };
+module.exports = { User, Background };
