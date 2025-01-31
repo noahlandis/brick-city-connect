e04ddd0a9c3fde37795cbb2f66b724018cad9b5f
Noah Landis <noahlandis980@gmail.com>
2025-01-30 22:51:38 -0500

    adding backgrounds is working

--- server/src/services/level-service.js
+++ server/src/services/level-service.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/index');
-
+const { Background } = require('../models/index');
+const { Op } = require('sequelize');
 /**
  * Reward the user with xp based on the duration of the time they spent in the video chat lobby
  * @param {*} userID 
@@ -8,6 +9,12 @@ const { User } = require('../models/index');
 async function rewardUser(userID, duration) {
   const user = await User.findByPk(userID);
   user.addXP(duration);
+  await user.addBackgrounds(await Background.findAll({ where: {
+      requiredLevel: {
+        [Op.lte]: user.level,
+      },
+    }})
+  );
 }
 
 module.exports = { rewardUser };
