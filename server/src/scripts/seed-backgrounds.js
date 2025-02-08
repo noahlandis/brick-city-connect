0577fcf5723bb43b822915cf9177a3bb2442bfde
Noah Landis <noahlandis980@gmail.com>
2025-02-08 18:30:27 -0500

    removed some backgrounds

--- server/src/scripts/seed-backgrounds.js
+++ server/src/scripts/seed-backgrounds.js
@@ -3,18 +3,20 @@ const { Background } = require('../models/index');
 (async () => {
   try {
     await Background.bulkCreate([
-      // use the order name, url, requiredLevel instead
-      // dont include the id
       { url: 'sau.png', name: 'SAU', requiredLevel: 2 },
-      { url: 'fountain.png', name: 'Fountain', requiredLevel: 3 },
-      { url: 'shed.png', name: 'Shed', requiredLevel: 4 },
-      { url: 'magic.png', name: 'Magic', requiredLevel: 5 },
-      { url: 'gene_polisseni.png', name: 'Gene Polisseni', requiredLevel: 6 },
-      { url: 'global_village.png', name: 'Global Village', requiredLevel: 7 },
-      { url: 'tiger_statue.png', name: 'Tiger Statue', requiredLevel: 8 },
-      { url: 'library.png', name: 'Library', requiredLevel: 9 },
-      { url: 'infinity_quad.png', name: 'Infinity Quad', requiredLevel: 10 },
-      { url: 'crossroads.png', name: 'Crossroads', requiredLevel: 11 },
+      { url: 'shed.png', name: 'Shed', requiredLevel: 3 },
+      { url: 'magic.png', name: 'Magic', requiredLevel: 4 },
+      { url: 'gene_polisseni.png', name: 'Gene Polisseni', requiredLevel: 5 },
+      { url: 'global_village.png', name: 'Global Village', requiredLevel: 6 },
+      { url: 'infinity_quad.png', name: 'Infinity Quad', requiredLevel: 7 },
+      { url: 'crossroads.png', name: 'Crossroads', requiredLevel: 8 },
+
+      // future backgrounds
+      // { url: 'library.png', name: 'Library', requiredLevel: 9 },
+      // { url: 'fountain.png', name: 'Fountain', requiredLevel: 10 },
+      // { url: 'tiger_statue.png', name: 'Tiger Statue', requiredLevel: 11 },
+
+
 
     ], {
       ignoreDuplicates: true,
