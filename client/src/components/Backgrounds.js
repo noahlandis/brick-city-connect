9841f0919136d4351113a20e647e65ad410aa5a7
Noah Landis <noahlandis980@gmail.com>
2025-02-07 22:21:58 -0500

    new background files

--- client/src/components/Backgrounds.js
+++ client/src/components/Backgrounds.js
@@ -7,6 +7,20 @@ function Backgrounds({ onSelect, selectedBackground, backgrounds }) {
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     const defaultBackground = { id: 0, url: null, name: 'None', locked: false };
     backgrounds = [defaultBackground, ...backgrounds];
+    // add 'fountain.jpg' to backgrounds
+    backgrounds.push({ id: 1, url: 'fountain.png', name: 'Fountain', locked: false });
+    backgrounds.push({ id: 2, url: 'shed.png', name: 'Shed', locked: false });
+    backgrounds.push({ id: 3, url: 'magic.png', name: 'Magic', locked: false });
+
+    backgrounds.push({ id: 4, url: 'gene_polisseni.jpeg', name: 'Gene Polisseni', locked: false });
+    backgrounds.push({ id: 5, url: 'crossroads.png', name: 'Crossroads', locked: false });
+    backgrounds.push({ id: 6, url: 'global_village.png', name: 'Global Village', locked: false });
+    backgrounds.push({ id: 7, url: 'tiger_statue.jpg', name: 'Tiger Statue', locked: false });
+    backgrounds.push({ id: 8, url: 'library.jpg', name: 'Library', locked: false });
+    backgrounds.push({ id: 9, url: 'infinity_quad.jpeg', name: 'Infinity Quad', locked: false });
+    backgrounds.push({ id: 10, url: 'sau.jpg', name: 'SAU', locked: false });
+    
+
 
     return (
         <Box
