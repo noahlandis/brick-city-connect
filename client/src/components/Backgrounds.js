3a952726cc5b4cbc5c691c8622d59906a7b80842
Noah Landis <noahlandis980@gmail.com>
2025-02-09 14:38:05 -0500

    increased background opacity

--- client/src/components/Backgrounds.js
+++ client/src/components/Backgrounds.js
@@ -64,7 +64,7 @@ function Backgrounds({ onSelect, selectedBackground, backgrounds }) {
                                 left: 0,
                                 width: '100%',
                                 height: '100%',
-                                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                                backgroundColor: 'rgba(0, 0, 0, 0.883)',
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
