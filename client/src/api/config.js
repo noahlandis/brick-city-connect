3870b99d74fb1a5593c8826d350f988c9dd84144
Noah Landis <noahlandis980@gmail.com>
2025-03-07 16:06:07 -0500

    fixed auth so it uses JWT

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+
 export const api = axios.create({
     baseURL: `${process.env.REACT_APP_SERVER_URL}/api`
 });
+
+export const authenticatedApi = (token) => {
+    return axios.create({
+        baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+};
