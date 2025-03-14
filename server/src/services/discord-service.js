becbccb37797d336ec8c574d6c210206db3c7445
Noah Landis <noahlandis980@gmail.com>
2025-03-14 13:03:00 -0400

    rewarding user bg after oauth

--- server/src/services/discord-service.js
+++ server/src/services/discord-service.js
@@ -1,4 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { User } = require('../models');
+const { giveUserDiscordBackground } = require('./reward-discord-background');
 
 async function initializeDiscordBot() {
     const client = new Client({
@@ -8,8 +10,23 @@ async function initializeDiscordBot() {
         ],
     });
 
-    client.on('guildMemberAdd', (member) => {
+    client.on('guildMemberAdd', async (member) => {
         console.log("User joined! Here is their discord ID: " + member.id);
+
+        // since a user joined, we find the user in our database and give them the Library background
+        // 
+        const user = await User.findOne({
+            where: {
+                discordId: member.id
+            }
+        });
+
+        if (!user) {
+            // in this case, this event is triggered by a user who joined the discord server without using our oauth flow, so we can't give them the  background
+            return;
+        }
+
+        await giveUserDiscordBackground(user);
     });
 
     await client.login(process.env.DISCORD_BOT_TOKEN);
